a354b81171c1388784736179038501e00cc68c64
Thomas-Gabriel-Petcu <73488732+Thomas-Gabriel-Petcu@users.noreply.github.com>
2022-01-21 15:20:21 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleScreen
+{
+    class Program
+    {
+        static Screen screen = new Screen();
+
+        static string[,] gamespace = new string[128,128];
+        static void Main(string[] args)
+        {
+            string[,] map = IterateGameSpace(gamespace);
+            screen.displayFrame(map);
+            Console.ReadKey();
+        }
+        public static string[,] IterateGameSpace(string[,] gamespace)
+        {
+            for (int x = 0; x < gamespace.GetLength(0); x++)
+            {
+                for (int y = 0; y < gamespace.GetLength(1); y++)
+                {
+                    gamespace[x, y] = $" {x},{y} ";
+                }
+            }
+            return gamespace;
+        } 
+    }
+}
---
+++ Screen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleScreen
+{
+    public class Screen
+    {
+        public void displayFrame(string[,] display)
+        {
+            string storage = "";
+            for (int x = 0; x < display.GetLength(0); x++)
+            {
+                for (int y = 0; y < display.GetLength(1); y++)
+                {
+                   storage += $"{display[x, y]}";
+                }
+               storage += System.Environment.NewLine;
+            }
+            Console.Write(storage);
+        }
+    }
+}
